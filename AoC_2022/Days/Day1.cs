ec988a232c91400a87392949458c41f924437f22
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-12-11 10:50:10 +0100

    Add solution for Day 1

--- AoC_2022/Days/Day1.cs
+++ AoC_2022/Days/Day1.cs
@@ -2,8 +2,32 @@
 
 public class Day1 : Day
 {
+    private static List<int> GetElfs(string puzzleInput)
+    {
+        var elfs = new List<int>();
+        int calories = 0;
+        string[] split = puzzleInput.Split("\r\n");
+        foreach (string item in split)
+        {
+            if (item == "")
+            {
+                elfs.Add(calories);
+                calories = 0;
+            }
+            else
+            {
+                calories += int.Parse(item);
+            }
+        }
+
+        return elfs;
+    }
+
     protected override void Run(string puzzleInput)
     {
+        List<int> elfs = GetElfs(puzzleInput);
+        int bestElf = elfs.Max();
 
+        Console.WriteLine($"Solution: {bestElf}");
     }
 }
