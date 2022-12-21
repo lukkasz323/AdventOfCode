be1ed18c398e04edcb937e514ae4e46776875ece
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-12-21 14:46:47 +0100

    Add: Day4

--- AoC_2022/Days/Day4.cs
+++ AoC_2022/Days/Day4.cs
@@ -4,6 +4,31 @@ public class Day4 : Day
 {
     protected override void Solve()
     {
-	    
+        int pairsFullyOverlapping = 0;
+        int pairsOverlapping = 0;
+
+        foreach (string line in PuzzleInput.Where(x => x != ""))
+		{
+            string[] elfPair = line.Split(',');
+            int[] range1 = Array.ConvertAll(elfPair[0].Split('-'), int.Parse);
+            int[] range2 = Array.ConvertAll(elfPair[1].Split('-'), int.Parse);
+            List<int> sectors1 = Enumerable.Range(range1[0], range1[1] - range1[0] + 1).ToList();
+            List<int> sectors2 = Enumerable.Range(range2[0], range2[1] - range2[0] + 1).ToList();
+
+            // Part 1
+            if (!sectors1.Except(sectors2).Any() || !sectors2.Except(sectors1).Any())
+            {
+                pairsFullyOverlapping += 1;
+            }
+
+            // Part 2
+            if (sectors1.Intersect(sectors2).Any() || sectors2.Intersect(sectors1).Any())
+            {
+                pairsOverlapping += 1;
+            }
+        }
+
+        Solution1 = pairsFullyOverlapping;
+        Solution2 = pairsOverlapping;
     }
 }
