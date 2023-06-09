cbb3bd698d18cc63fb15f9f46cea2f0020c5222d
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-06-10 00:07:26 +0200

    Finish Day 6

--- AoC_2022/Days/Day6.cs
+++ AoC_2022/Days/Day6.cs
@@ -4,6 +4,31 @@ public class Day6 : Day
 {
     protected override void Solve()
     {
+        string datastream = PuzzleInput[0];
 
+        Solution1 = FindMarker(datastream, 4);
+        Solution2 = FindMarker(datastream, 14);
+    }
+
+    private static int FindMarker(string datastream, int requiredMarkerLength) 
+    {
+        var marker = new List<char>();
+
+        for (int i = 0; i < datastream.Length; i++)
+        {
+            char ch = datastream[i];
+
+            if (marker.Contains(ch))
+            {
+                marker.RemoveRange(0, marker.IndexOf(ch) + 1);
+            }
+            marker.Add(ch);
+            if (marker.Count == requiredMarkerLength)
+            {
+                return i + 1;
+            }
+        }
+
+        throw new InvalidOperationException($"The marker was not found.");
     }
 }
